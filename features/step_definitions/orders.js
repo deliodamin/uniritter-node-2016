52305d7bea5afad1eb462e02a3fb099c1e86af2b
Delio Damin <deliodamin@hotmail.com>
2016-06-18 14:11:02 +0000

    Inicio testes Cenários

--- features/step_definitions/orders.js
+++ features/step_definitions/orders.js
@@ -43,4 +43,33 @@ module.exports = function () {
     this.Then(/^its status is (.*)$/, function (status) {
         expect(this.responseBody.data.attributes.status).to.equal(status);
     });
+    
+    
+   
+   
+   
+   
+   
+   
+    this.Given(/^a valid order$/, function () {
+        const 
+            that = this,
+            payload = {
+            data: {
+                type: 'orders',
+                attributes: {
+                    items: [{ product_id: '598b04ea-8c20-4240-9c2b-1d36350a8d33', quantity: 1}]
+                    }
+                }
+            }
+        
+
+        return this.doHttpRequest('orders', 'post', payload)
+        .then((response) => {
+            that.existingOrder = response.body;
+            return response;
+        });
+    });
+    
+    
 }
